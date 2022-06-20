9d4516f43c0382d2890e74bd851d44eba558240b
anacgms <acgomes43@gmail.com>
2022-06-20 11:08:37 -0300

    desafio

---
+++ script.js
@@ -0,0 +1,19 @@
+let nome='Ana'
+console.log (nome)
+
+let number=26
+console.log (number)
+
+let actualYear= 2022 - number
+console.log (actualYear)
+
+let soma= actualYear + number
+console.log (soma)
+
+let text='Estou mexendo no console.log'
+console.log (text)
+
+let wednesday= false
+console.log (wednesday)
+
+console.log (typeof wednesday)
